38d8f2bcb5edcb0a71e7368f88a2b2e2730cab76
mjidelouss <mjid.elouss@gmail.com>
2024-07-23 14:53:22 +0200

    Fixed colors

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -3,10 +3,10 @@ import { angkor } from "./fonts";
 const Header = () => {
     return (
       <div className="text-center mb-4">
-        <p className={` text-xl text-gray-600 dark:text-gray-300 ${angkor.className} `}>
-        Calculate Your <span className="text-blue-300">Savings </span> Goals Based on <span className="text-yellow-300">Salary</span>  and <span className="text-red-300">Expenses</span> 
+        <p className={` text-xl text-gray-700 dark:text-gray-300 ${angkor.className} `}>
+        Calculate Your <span className="text-blue-400">Savings </span> Goals Based on <span className="text-yellow-400">Salary</span>  and <span className="text-red-400">Expenses</span> 
         </p>
-        <p className={`text-xl text-gray-600 dark:text-gray-300 ${angkor.className} `}>to Reach Your Desired <span className="text-purple-300">Amount</span>  Within Your Chosen <span className="text-gray-300">Timeframe</span>.</p>
+        <p className={`text-xl text-gray-700 dark:text-gray-300 ${angkor.className} `}>to Reach Your Desired <span className="text-purple-400">Amount</span>  Within Your Chosen <span className="text-gray-400">Timeframe</span>.</p>
       </div>
     );
   };
